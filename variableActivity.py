0f9cba0f15820beac8cbdb86969b1a8239ba9771
sul <rmeadows024@boyslatin.net>
2026-09-03 15:05:28 +0000

    variable activity for class grade

---
+++ variableActivity.py
@@ -0,0 +1,72 @@
+ # VARIABLES - a container that stores data.
+ # you can name your container almost anything
+ # you want, so long as it descriptive.
+
+ appleCost.00
+numberOfApple = 10
+
+Ja3SneakerCount = 10
+Ja3SneakerPrice = 150.99
+Ja3SneakersSku = 91022382913
+
+
+orange = 1.05
+ user = "Rahsul Meadows"
+ hasfriedOnions = True
+
+ # Variable Naming Rules
+ # 1. A variable MUST start with letter and cannot start with a number
+ # 2. A variable can be a mix of letters and numbers But not symbols
+ # 3. A variable cannot be a pthon keyword
+ # 4. Variables with multiple words does not have spaces
+
+
+ # apple store
+
+AS = 3
+applesAtStore = 3
+
+ # math class
+
+ mc = 17
+studentsInClass = 17
+
+ # travel insurance
+
+ on/off
+
+ # membership for gym
+
+ lowClass/MiddleClass/HighClass
+ Noob/Pro/Master
+
+ # drink menu
+
+ commonDrinks(sprite) exoticDrinks(dragonfruit blast)
+FountainDrinks = 2.79
+BartenderDrinks = 4.69
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
